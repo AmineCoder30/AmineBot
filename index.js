c62f883786c0737842cd536dfb81ff2b11ea7470
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2024-02-18 19:35:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
+// index.js
 import TelegramBot from "node-telegram-bot-api";
 import questionList from "./Question.js";
-import http  from "http"
+import http from "http";
 import dotenv from "dotenv";
 dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const port = process.env.PORT
-const bot = new TelegramBot(token, { polling: true });
+const port = process.env.PORT;
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(port);
+const bot = new TelegramBot(token);
+const server = http.createServer(async (req, res) => {
+  if (req.method === "POST" && req.url === "/webhook") {
+    await bot.processUpdate(req.body);
+    res.statusCode = 200;
+    res.write("OK");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write("Not Found");
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  bot.setWebHook(`https://amine-bot.vercel.app/webhook`);
+  console.log(`Server is listening on port ${port}`);
+});
 
 bot.on("message", (msg) => {
   const item = questionList[Math.floor(Math.random() * questionList.length)];
   const chatId = msg.chat.id;
   const messageText = msg.text;
-  const message_id = msg.message_id;
 
   if (messageText === "اسألني") {
-    bot.sendMessage(chatId, `${item}؟`, {
-      reply_to_message_id: message_id,
-    });
+    bot.sendMessage(chatId, `${item}؟`);
   }
 });
