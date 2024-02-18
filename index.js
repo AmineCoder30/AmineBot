2250681d51a5723b02ee35619c308edd1fcdab0c
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2024-02-18 18:17:34 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import TelegramBot from "node-telegram-bot-api";
 import questionList from "./Question.js";
+import http  from "http"
 import dotenv from "dotenv";
 dotenv.config();
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const port = process.env.PORT
 const bot = new TelegramBot(token, { polling: true });
 
-
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(port);
 
 bot.on("message", (msg) => {
   const item = questionList[Math.floor(Math.random() * questionList.length)];
